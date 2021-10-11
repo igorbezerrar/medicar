3cf736cde8278a9ac6422cf8e15f60672dfcb367
Igor Bezerra <igorbezerra234@gmail.com>
2021-10-11 15:02:39 -0300

    Update Final

--- medicar_br/settings.py
+++ medicar_br/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_filters',
     'rest_framework',
     'multiselectfield',
     'rest_framework.authtoken',
